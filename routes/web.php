2eb8d97ba6ea50810d298ab41dbbba871c9f3c54
nekoyasha2512 <46078151+nekoyasha2512@users.noreply.github.com>
2026-09-18 20:02:53 +0800

    Remove unused routes from web.php
    Removed unused routes for encounter details and server time.

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DpsController@overviewPage');
-
-Route::get('/encounter/{stat}', function (Stat $stat) {
-    return view('encounter', ['stat' => $stat]);
-})->name('statDetail');
-
-Route::get('/shared/servertime', function () {
-    return response()->json(['serverTime' => time()]);
-});
 // 接收 TERA DPS Meter 戰鬥數據的 POST 路由
 Route::post('/dps', 'DpsController@store');
 Route::get('/dps', function () {
