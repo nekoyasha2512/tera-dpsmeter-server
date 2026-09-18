2ba01af9cd8a34be73b6ce3db9fbda97bb023c39
nekoyasha2512 <46078151+nekoyasha2512@users.noreply.github.com>
2026-09-18 21:12:25 +0800

    Refactor database options for SSL configuration

--- config/database.php
+++ config/database.php
@@ -54,9 +54,8 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => [
-                PDO::MYSQL_ATTR_SSL_CA => NULL,
-                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
 
