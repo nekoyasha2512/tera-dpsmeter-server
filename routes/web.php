31d382c0bd7b1a6f3bafbbacb3f04970936db8f5
nekoyasha2512 <46078151+nekoyasha2512@users.noreply.github.com>
2026-09-18 14:05:53 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,17 @@ Route::get('/encounter/{stat}', function (Stat $stat) {
 Route::get('/shared/servertime', function () {
     return response()->json(['serverTime' => time()]);
 });
+Route::get('/init-db', function () {
+    try {
+        // 強制清除快取
+        \Artisan::call('config:clear');
+        \Artisan::call('cache:clear');
+        
+        // 強制執行 Migration 建表
+        \Artisan::call('migrate', ['--force' => true]);
+        
+        return '<h1>資料庫連線與 Migration 成功！</h1><pre>' . \Artisan::output() . '</pre>';
+    } catch (\Exception $e) {
+        return '<h1>失敗原因：</h1><pre>' . $e->getMessage() . '</pre>';
+    }
+});
