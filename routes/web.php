83c38e6f3ee91a7b3c360fa0ced0924819e042ea
nekoyasha2512 <46078151+nekoyasha2512@users.noreply.github.com>
2026-09-18 20:07:52 +0800

    Update routes to use StatsController and GlyphController

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// 首頁 GET 請求顯示網頁
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// 接收 TERA DPS Meter 的 POST 上傳請求（直接對應根目錄）
-Route::post('/', 'DpsController@store');
+Route::get('/', 'StatsController@index');
+Route::post('/dps', 'StatsController@store');
+Route::post('/glyph', 'GlyphController@store');
