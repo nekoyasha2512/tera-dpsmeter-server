d00d4c8d6dd09d2e7cbaf8818c2ddf32dd0f06c6
nekoyasha2512 <46078151+nekoyasha2512@users.noreply.github.com>
2026-09-18 20:33:41 +0800

    Add health check route for API status

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,9 @@
 <?php
 use App\Http\Controllers\StatsController;
-
+Route::get('/', function () {
+    return response()->json([
+        'status' => 'online',
+        'message' => 'TERA DPS Meter Database API is running.'
+    ]);
+});
 Route::get('/stats', [StatsController::class, 'index']);
