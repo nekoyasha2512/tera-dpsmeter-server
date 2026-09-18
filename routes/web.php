6675a04dd8379ed9c546472bcb4ab9ccfe4f5a4c
nekoyasha2512 <46078151+nekoyasha2512@users.noreply.github.com>
2026-09-18 20:14:14 +0800

    Add routes for StatsController and GlyphController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StatsController;
+use App\Http\Controllers\GlyphController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
 Route::get('/', 'StatsController@index');
 Route::post('/dps', 'StatsController@store');
 Route::post('/glyph', 'GlyphController@store');
