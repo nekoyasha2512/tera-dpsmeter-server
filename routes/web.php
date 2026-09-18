4e3b05fc525336b5abac13c1536d608fded31055
nekoyasha2512 <46078151+nekoyasha2512@users.noreply.github.com>
2026-09-18 19:57:53 +0800

    Add DPS Meter POST route and status GET route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// 接收 TERA DPS Meter 戰鬥數據的 POST 路由
+Route::post('/dps', 'DpsController@store');
+Route::get('/dps', function () {
+    return response()->json(['status' => 'DPS Server Active']);
+});
